8833522175a24e4bd6f333fc4c9adc03718a4014
darkendris23 <42562055+darkendris23@users.noreply.github.com>
2020-02-07 11:08:44 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const TOKEN = process.env.TOKEN;
+const settings =  require ('./settings')
+
+bot.commands = new Discord.Collection();
+const botCommands = require('./commands');
+
+client.on('ready', () => {
+    console.log("Connected as " + client.user.tag)
+	console.log("Servers:")
+    client.guilds.forEach((guild) => {
+        console.log(" - " + guild.name)
+		
+		 // List all channels
+        guild.channels.forEach((channel) => {
+            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+        })
+})
+
+client.login(TOKEN)
