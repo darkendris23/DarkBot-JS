595bcfe0e51eebde2d0c172626fcc8f33afcb114
darkendris23 <42562055+darkendris23@users.noreply.github.com>
2020-02-07 11:25:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const Discord = require('discord.js')
-const client = new Discord.Client()
 
 const TOKEN = process.env.TOKEN;
 const settings =  require ('./settings.json')
 
 bot.commands = new Discord.Collection();
 const botCommands = require('./commands');
+
+const client = new Discord.Client()
+
+//------------------------------------------------//
 
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag)
@@ -19,5 +22,7 @@ client.on('ready', () => {
             console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
         })
 })
+
+//------------------------------------------------//
 
 client.login(TOKEN)
