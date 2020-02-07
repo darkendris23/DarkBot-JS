39906fad8c0573fd8d2046bd36bf1cf6f6f90e38
darkendris23 <42562055+darkendris23@users.noreply.github.com>
2020-02-07 11:30:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Discord = require('discord.js')
 
 const TOKEN = process.env.TOKEN;
-const settings =  require ('./settings.json')
+const { Prefix } =  require ('./settings.json')
 
 bot.commands = new Discord.Collection();
 const botCommands = require('./commands');
@@ -22,6 +22,13 @@ client.on('ready', () => {
             console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
         })
 })
+
+// client.on('message', message => {
+if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).split(' ');
+const command = args.shift().toLowerCase();
+// the rest of your code
 
 //------------------------------------------------//
 
