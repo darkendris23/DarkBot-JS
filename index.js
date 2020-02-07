c5d47b518001dc2458eb1d6445e3e761b20f9eb4
darkendris23 <42562055+darkendris23@users.noreply.github.com>
2020-02-07 11:32:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ if (!message.content.startsWith(prefix) || message.author.bot) return;
 const args = message.content.slice(prefix.length).split(' ');
 const command = args.shift().toLowerCase();
 // the rest of your code
+// using the new `command` variable, this makes it easier to manage!
+// you can switch your other commands to this format as well
+else if (command === 'args-info') {
+	if (!args.length) {
+		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+	}
+
+	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+}
 
 //------------------------------------------------//
 
