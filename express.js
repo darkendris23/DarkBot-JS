cf1d13e690f472f1f970a7134b39625565540026
Glitch (darkendris23-darkbot) <none>
2020-02-08 05:01:01 +0000

    🏎🎙 Updated with Glitch

--- express.js
+++ express.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const express = require('express');
+const http = require("http");
+const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping Received");
--- index.js
+++ index.js
@@ -1,45 +1,61 @@
 require('dotenv').config();
 const fs = require('fs');
-const Discord = require('discord.js')
-
-const { prefix } =  require ('./settings.json');
-const botCommands = require('./commands');
+const Enmap = require("enmap");
+const Discord = require('discord.js');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+const config = require('./settings.json');
+client.config = config;
+
+const botCommands = require('./commands');
+
+const prefix = config.prefix;
+
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
 
-//------------------------------------------------//
+    //------------------------------------------------//
 
-client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
-	console.log("Servers:")
-    client.guilds.forEach((guild) => {
-        console.log(" - " + guild.name)
-		
-		 // List all channels
-        guild.channels.forEach((channel) => {
-            console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+    client.on('ready', () => {
+        console.log("Connected as " + client.user.tag)
+        console.log("Servers:")
+        client.guilds.forEach((guild) => {
+            console.log(" - " + guild.name);
+          
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+
+            // List all channels
+            guild.channels.forEach((channel) => {
+                console.log(` -- ${channel.name} (${channel.type}) - ${channel.id}`)
+            })
         })
-})
+    });
 
-// client.on('message', message => {
-if (!message.content.startsWith(prefix) || message.author.bot) return;
+    client.on('message', message => {
+        if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-const args = message.content.slice(prefix.length).split(/ +/);
-const command = args.shift().toLowerCase();
-// the rest of your code
-// using the new `command` variable, this makes it easier to manage!
-// you can switch your other commands to this format as well
-if (!client.commands.has(command)) return;
-try {
-client.commands.get(command).execute(message, args);	} catch (error) {		console.error(error);		message.reply('there was an error trying to execute that command!');	}});
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toLowerCase();
+        console.info(`Called command: ${command}`);
+        // the rest of your code
+        // using the new `command` variable, this makes it easier to manage!
+        // you can switch your other commands to this format as well
+        if (!client.commands.has(command)) return;
+      
+        try {
+            client.commands.get(command).execute(message, args);
+        } catch (error) {
+            console.error(error);
+            msg.reply('there was an error trying to execute that command!');
+        }
+    });
 
-//------------------------------------------------//
+    //------------------------------------------------//
 
-client.login(process.env.DISCORD_TOKEN);
+    client.login(config.token);
